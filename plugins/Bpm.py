e4c1fdd05b7e4a770ee18bb2fc0856a3d2f38a2e
Laurent Claustre <claustre@linuxlolo1.esrf.fr>
2019-11-28 08:18:18 +0100

    reset min/max to default if 0/0

--- plugins/Bpm.py
+++ plugins/Bpm.py
@@ -440,6 +440,10 @@ class BpmDeviceServer(BasePostProcess):
         data = attr.get_write_value()
         max = 2**(self.ImageType2Bpp[_control_ref().image().getImageType()])
         range = [0, max]
+        #reset asked to default
+        if data[0] == 0 and data[1] == 0:
+            data[1] = max
+
         if data[0] > data[1]:
             PyTango.Except.throw_exception('WrongData',\
                                            f'Wrong values min_max: {data}, max < min',
@@ -466,7 +470,7 @@ class BpmDeviceServer(BasePostProcess):
         data = attr.get_write_value()
         self.return_bpm_profiles = data
         #update the property
-        prop = {'return_bpm_profiles': data}
+        prop = {'return_bpmpylon/PylonBase.h_profiles': data}
         PyTango.Database().put_device_property(self.get_name(), prop)
 
     def is_return_bpm_profiles_allowed(self,mode) :
